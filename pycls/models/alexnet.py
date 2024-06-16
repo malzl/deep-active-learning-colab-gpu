8f3c0ee4a016a01253c2595bfbe2ea770af88a2c
Nicolas Malz <n.malz@campus.lmu.de>
2024-06-16 22:15:31 +0200

    updated cnn

--- pycls/models/alexnet.py
+++ pycls/models/alexnet.py
@@ -3,9 +3,7 @@ import torch.nn as nn
 from torch.utils.model_zoo import load_url as load_state_dict_from_url
 from typing import Any
 
-
 __all__ = ['AlexNet', 'alexnet']
-
 
 model_urls = {
     'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
@@ -20,10 +18,8 @@ class AlexNet(nn.Module):
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         self.drop1 = nn.Dropout(0.25)
         self.flatten = nn.Flatten()
-        # Adjusted for 32x32 input size, after pooling the size will be 16x16
-        self.fc1 = nn.Linear(128 * 8 * 8, 128)
-        self.drop2 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(128, num_classes)
+        self.fc1 = nn.Linear(128 * 8 * 8, num_classes)
+        self.softmax = nn.Softmax(dim=1)  # Softmax layer
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = torch.relu(self.conv1(x))
@@ -31,64 +27,10 @@ class AlexNet(nn.Module):
         x = self.pool(x)
         if self.use_dropout:
             x = self.drop1(x)
-        # print(f"Shape before flatten: {x.shape}")  # Debug print to verify shape
         x = self.flatten(x)
-        x = torch.relu(self.fc1(x))
-        if self.use_dropout:
-            x = self.drop2(x)
-        x = self.fc2(x)
+        x = self.fc1(x)
+        x = self.softmax(x)  # Applying softmax
         return x
-
-
-# class AlexNet(nn.Module):
-#     '''
-#     AlexNet modified (features) for CIFAR10. Source: https://github.com/icpm/pytorch-cifar10/blob/master/models/AlexNet.py. 
-#     '''
-#     def __init__(self, num_classes: int = 1000, use_dropout=False) -> None:
-#         super(AlexNet, self).__init__()
-#         self.use_dropout = use_dropout
-#         self.features = nn.Sequential(
-#             nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=2),
-#             nn.Conv2d(64, 192, kernel_size=3, padding=1),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=2),
-#             nn.Conv2d(192, 384, kernel_size=3, padding=1),
-#             nn.ReLU(inplace=True),
-#             nn.Conv2d(384, 256, kernel_size=3, padding=1),
-#             nn.ReLU(inplace=True),
-#             nn.Conv2d(256, 256, kernel_size=3, padding=1),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=2),
-#         )
-#         # self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-#         self.fc_block = nn.Sequential(
-#             nn.Linear(256 * 2 * 2, 4096, bias=False),
-#             nn.BatchNorm1d(4096),
-#             nn.ReLU(inplace=True),
-#             nn.Linear(4096, 4096, bias=False),
-#             nn.BatchNorm1d(4096),
-#             nn.ReLU(inplace=True),
-#         )
-#         self.classifier = nn.Sequential(
-#             nn.Linear(4096, num_classes),
-#         )
-#         self.penultimate_active = False
-#         self.drop = nn.Dropout(p=0.5)
-
-#     def forward(self, x: torch.Tensor) -> torch.Tensor:
-#         x = self.features(x)
-#         # x = self.avgpool(x)
-#         z = torch.flatten(x, 1)
-#         if self.use_dropout:
-#             x = self.drop(x)
-#         z = self.fc_block(z)
-#         x = self.classifier(z)
-#         if self.penultimate_active:
-#             return z, x
-#         return x
-
 
 def alexnet(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> AlexNet:
     r"""AlexNet model architecture from the
